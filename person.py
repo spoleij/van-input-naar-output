96ec665a0473097e348ac4d1c9cf3cd18df82fc6
Steph Poleij <99033734@mydavinci.nl>
2021-09-07 00:08:36 +0200

    adreskaart

---
+++ person.py
@@ -0,0 +1,23 @@
+#opdracht: toon naam, adres, postcode en woonplaats met prompts
+
+print ('Hallo daar generaal kenobi')
+naam = input ('Hoe heet je?:   ')
+adres = input ('Wat is je adres?:   ')
+postcode = input ('Wat is je postcode?:   ')
+woonplaats = input ('En je woonplaats?:   ')
+
+
+#adreskaart voorbeeld:
+#----------------------------------------------------
+#|  Naam      : Fabian van Zandt                
+#|  Adres     : Koekenbakkersplein 23           
+#|  Postcode  : 2834 EF                         
+#|  Woonplaats: Houten                          
+# ----------------------------------------------------
+ 
+print ('----------------------------------------------------')
+print ('|  Naam      : ' + naam                )
+print ('|  Adres     : ' + adres               )
+print ('|  Postcode  : ' + postcode            )
+print ('|  Woonplaats: ' + woonplaats          )
+print ('----------------------------------------------------')
